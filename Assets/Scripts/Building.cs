a88890b8c876b1488efd7f3d172b99896a890304
TheTrueCyprien <connor95@hotmail.de>
2020-05-25 22:11:20 +0200

    Added Building.cs

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
+    public enum BuildingType
+    {
+        Base, Fishery, Lumberjack, Sawmill, SheepFarm, Weavery, PotatoFarm, SchnappsDistillery
+    }
+
+    public BuildingType type;
+    public int upkeep;
+    public int cost_money;
+    public int cost_planks;
+    public Tile tile;
+    public int generation_interval;
+    public int output_count;
+    public List<Tile.TileTypes> buildable_on;
+    public Tile.TileTypes scales_with;
+    public Vector2Int neighbour_range;
+    public List<GameManager.ResourceTypes> input_ressources;
+    public GameManager.ResourceTypes output_ressources;
+
+    private float efficiency = 1.0f;
+    private float progress = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
